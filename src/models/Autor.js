6387b4e8014db210f74a3d3f9707ef3e446feaff
Bergson Brito Barros <bergson.barros@serpro.gov.br>
2025-05-30 17:20:04 -0300

    Comenta a definição do campo _id no modelo de autor: ajusta a estrutura do esquema para melhor legibilidade.

--- src/models/Autor.js
+++ src/models/Autor.js
@@ -3,12 +3,16 @@ import mongoose from "mongoose";
 const autorSchema = new mongoose.Schema(
   {
     id: {type: String},
-    _id: {type: mongoose.Schema.Types.ObjectId},
+    //_id: {type: mongoose.Schema.Types.ObjectId},
     nome: {
       type: String,
       required: [true, "O nome do(a) autor(a) é obrigatório"]
     },
-    nacionalidade: {type: String}
+    nacionalidade: {type: String},
+    imagem: {
+      type: String,
+      required: [false, "A imagem do(a) autor(a) não é obrigatória"],
+    }
   }
 );
 
